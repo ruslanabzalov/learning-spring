7f08705bae8d6a08ba40547d2bce2868be846e17
Ruslan Abzalov <ruslanonetwo@gmail.com>
2020-11-02 17:15:36 +0300

    Cats/Dogs Sample of the Spring DI

--- sfg/sfg-di/src/main/java/abzalov/ruslan/sfgdi/SfgDiApplication.java
+++ sfg/sfg-di/src/main/java/abzalov/ruslan/sfgdi/SfgDiApplication.java
@@ -11,6 +11,10 @@ public class SfgDiApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
+		System.out.println("---------------- The Best Pet is ----------------");
+		PetController petController = ctx.getBean("petController", PetController.class);
+		System.out.println(petController.whichPetIsTheBest());
+
 		System.out.println("---------------- Profiles ----------------");
 		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
 		System.out.println(i18nController.sayHello());
