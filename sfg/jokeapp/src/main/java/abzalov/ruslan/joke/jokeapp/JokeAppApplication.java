6df59a7ee504e4beca9f7c0621e03d97c66d43e9
Ruslan Abzalov <ruslanonetwo@gmail.com>
2020-11-24 17:17:10 +0300

    Created Jokes App

---
+++ sfg/jokeapp/src/main/java/abzalov/ruslan/joke/jokeapp/JokeAppApplication.java
@@ -0,0 +1,12 @@
+package abzalov.ruslan.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeAppApplication.class, args);
+    }
+}
