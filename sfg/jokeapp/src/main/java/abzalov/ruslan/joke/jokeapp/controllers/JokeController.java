bdc11665a0449072de2bea8c599cd71156481f6b
Ruslan Abzalov <ruslanonetwo@gmail.com>
2020-11-25 12:01:23 +0300

    Added Thymeleaf Template

--- sfg/jokeapp/src/main/java/abzalov/ruslan/joke/jokeapp/controllers/JokeController.java
+++ sfg/jokeapp/src/main/java/abzalov/ruslan/joke/jokeapp/controllers/JokeController.java
@@ -16,7 +16,7 @@ public class JokeController {
 
     @RequestMapping({"/", ""})
     public String showJoke(Model model) {
-        model.addAttribute(jokeService.getJoke());
+        model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
 }
