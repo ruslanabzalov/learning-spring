20f413adde4a589bec4046bc9ab3e19958eec830
Ruslan Abzalov <ruslanonetwo@gmail.com>
2020-11-25 11:46:28 +0300

    Added Service and Controller

---
+++ sfg/jokeapp/src/main/java/abzalov/ruslan/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package abzalov.ruslan.joke.jokeapp.controllers;
+
+import abzalov.ruslan.joke.jokeapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute(jokeService.getJoke());
+        return "chucknorris";
+    }
+}
---
+++ sfg/jokeapp/src/main/java/abzalov/ruslan/joke/jokeapp/services/JokeService.java
@@ -0,0 +1,6 @@
+package abzalov.ruslan.joke.jokeapp.services;
+
+public interface JokeService {
+
+    String getJoke();
+}
